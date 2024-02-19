4ba383793a47f6a98ece1759626e470d0b37d58b
buffeddude69 <tolaybagz@gmail.com>
2024-02-19 18:19:41 +0800

    HOMEPAGE
    food nutrition facts

--- App1/App1/HomePage.cs
+++ App1/App1/HomePage.cs
@@ -23,6 +23,9 @@ namespace App1
         ListView lv;
         ArrayList foods;
         ArrayAdapter _adapter;
+        TextView nutriContentTV;
+        Button addbtn;
+        LinearLayout ll;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -33,6 +36,10 @@ namespace App1
             lv = FindViewById<ListView>(Resource.Id.listview1);
             sv = FindViewById<SearchView>(Resource.Id.searchfood);
 
+            ll = FindViewById<LinearLayout>(Resource.Id.linearLayout2);
+            nutriContentTV = FindViewById<TextView>(Resource.Id.nutritionContentTV);
+            addbtn = FindViewById<Button>(Resource.Id.myButton);
+
             addData();
 
             _adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, foods);
@@ -40,19 +47,37 @@ namespace App1
 
             sv.QueryTextChange += sv_QueryTextChange;
             lv.ItemClick += lv_ItemClick;
-           
+
         }
 
         private void lv_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
         {
-            Toast.MakeText(this, _adapter.GetItem(e.Position).ToString(),ToastLength.Short).Show();
+            string selectedFood = _adapter.GetItem(e.Position).ToString();
+            updatenutritionalContent(selectedFood);
         }
 
         private void sv_QueryTextChange(object sender, SearchView.QueryTextChangeEventArgs e)
         {
             _adapter.Filter.InvokeFilter(e.NewText);
+
         }
 
+        private void updatenutritionalContent(string food)
+        {
+            string nutritionalcontent = getNutritionalContent(food);
+            nutriContentTV.Text = nutritionalcontent;
+            ll.Visibility = ViewStates.Visible;
+        }
+
+        private string getNutritionalContent(string food)
+        {
+            if (food.Equals("Chicken Breast Fillet"))
+            {
+                return "Chicken Breast Fillet: \n Calories: 120 \n Protein: 25g \n Fat: 2g \n Carbohydrates: 0g";
+            }
+
+            return "Nutritional content not available";
+        }
         private void addData()
         {
             foods = new ArrayList();
--- App1/App1/obj/Debug/130/android/src/androidx/activity/R.java
+++ App1/App1/obj/Debug/130/android/src/androidx/activity/R.java
@@ -8,7 +8,7 @@ package androidx.activity;
 
 public final class R {
 	public static final class id {
-		public static final int report_drawn = 0x7f08019c;
-		public static final int view_tree_on_back_pressed_dispatcher_owner = 0x7f080235;
+		public static final int report_drawn = 0x7f08019e;
+		public static final int view_tree_on_back_pressed_dispatcher_owner = 0x7f080237;
 	}
 }
